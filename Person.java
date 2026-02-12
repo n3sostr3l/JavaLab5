c6472a8eb30b0513464312002b126af6a4899b72
n3sostr3l <gladv242@gmail.com>
2026-02-12 17:35:23 +0300

    иди уже в свою ветку

--- Person.java
+++ Person.java
@@ -1,4 +1,5 @@
-public class Coordinates {
-    private Integer x; //Значение поля должно быть больше -881, Поле не может быть null
-    private Long y; //Поле не может быть null
+public class Person {
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private java.util.Date birthday; //Поле может быть null
+    private Location location; //Поле может быть null
 }
