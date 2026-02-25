152e3bf943e9313aff37c3118a26518618792d38
AIra <AIra.work213@gmail.com>
2026-02-25 14:28:36 +0300

    Обновлен FileEditor, добавлено возвращение ддаты

--- src/main/java/com/akira/FileEditor.java
+++ src/main/java/com/akira/FileEditor.java
@@ -3,43 +3,61 @@ package com.akira;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
 import java.util.Hashtable;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 
 public class FileEditor {
-    XmlMapper xmlMapper = new XmlMapper();
-    public void getCollection() {
+    private static final String DATA_FILE_NAME = "data.xml";
+    private final XmlMapper xmlMapper = new XmlMapper();
 
-       try {
-           // Читаем XML файл прямо в Hashtable
-           File file = new File("data.xml");
+    public Hashtable<String, Object> getCollection() {
 
-           try (InputStream inputStream = new FileInputStream(file);
-                InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8")) {
-               Hashtable<String, Object> result = xmlMapper.readValue(reader, Hashtable.class);
-               System.out.println("Данные из XML: " + result);
-           } catch (FileNotFoundException e) {
-               System.err.println("Файл не найден");
-           }
+        try {
+            File file = new File(DATA_FILE_NAME);
 
-        
-       } catch (IOException e) {
-           System.err.println("Ошибка работы с файлом: " + e.getMessage());
-       }
+            try (InputStream inputStream = new FileInputStream(file);
+                 InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+                Hashtable<String, Object> result = xmlMapper.readValue(reader, Hashtable.class);
+                System.out.println("Данные из XML: " + result);
+                return result;
+            } catch (FileNotFoundException e) {
+                System.err.println("Файл не найден");
+            }
+
+            return new Hashtable<>();
+        } catch (IOException e) {
+            System.err.println("Ошибка работы с файлом: " + e.getMessage());
+            return new Hashtable<>();
+        }
     }
-    public void updateCollection(Hashtable<String, Object> coll){
-        try (FileWriter fileWriter = new FileWriter("data.xml")) {
-                xmlMapper.writeValue(fileWriter, coll);
-        } catch (IOException e){
+
+    public void updateCollection(Hashtable<String, Object> coll) {
+        try (OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(Path.of(DATA_FILE_NAME)), StandardCharsets.UTF_8)) {
+            xmlMapper.writeValue(writer, coll);
+        } catch (IOException e) {
             System.err.println("Ошибка записи в файл");
         }
+    }
 
+    public Date getCollectionCreateTime() {
+        try {
+            BasicFileAttributes fileAttributes = Files.readAttributes(Path.of(DATA_FILE_NAME), BasicFileAttributes.class);
+            return new Date(fileAttributes.creationTime().toMillis());
+        } catch (IOException e) {
+            System.err.println("Ошибка получения даты создания файла: " + e.getMessage());
+            return null;
+        }
     }
 
 }
