f103039a82ee346c405d980f552e58a4512f77ce
n3sostr3l <gladv242@gmail.com>
2026-04-08 03:04:29 +0300

    ы-ы, minor fixes

--- src/main/java/com/akira/server/commands/CheckCommand.java
+++ src/main/java/com/akira/server/commands/CheckCommand.java
@@ -7,21 +7,41 @@ import com.akira.server.commands.interfaces.Command;
 import com.akira.server.commands.interfaces.Modable;
 import com.akira.server.commands.interfaces.SystemCommand;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class CheckCommand implements SystemCommand, Modable {
+public class CheckCommand implements Command, Modable {
     private ArrayList<String> args = new ArrayList<>();
 
     @Override
     public Response execute(CollectionManager collectionManager) {
+        String insertKey = CommandInvoker.getCommandsMap().entrySet().stream()
+                .filter(e -> e.getValue() instanceof InsertCommand)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
+
+//        List<String> replaceKeys = CommandInvoker.getCommandsMap().entrySet().stream()
+//                .filter(e -> (e.getValue() instanceof ReplaceLowestCommand) || (e.getValue() instanceof ReplaceGreatestCommand) )
+//                .map(Map.Entry::getKey)
+//                .toList();
+
         String updateKey = CommandInvoker.getCommandsMap().entrySet().stream()
                 .filter(e -> e.getValue() instanceof UpdateCommand )
                 .map(Map.Entry::getKey)
                 .findFirst()
                 .get();
-        if(args.get(0).equals(updateKey)) return ifIdIsTaken()?new Response("id занят",true):new Response("id свободен", true);
-        return new Response("",true);
+
+        String karg = "key";
+        if(args.get(0).equals(insertKey)) return new Response("",true);
+        if(args.get(0).equals(updateKey)) karg = "id";
+        return switch (karg){
+            case "id" -> new Response(ifIdIsTaken()?"id занят":"id свободен", true);
+            case "key" -> new Response(ifKeyIsTaken()?"ключ занят":"ключ свободен", true);
+            default -> new Response("",true);
+        };
     }
 
     @Override
@@ -41,10 +61,18 @@ public class CheckCommand implements SystemCommand, Modable {
 
     private boolean ifIdIsTaken(){
         Long id = Long.parseLong(args.get(1));
-        if(!CollectionManager.getCollection().entrySet().stream()
+        if(CollectionManager.getCollection().entrySet().stream()
                 .filter(integerLabWorkEntry -> integerLabWorkEntry.getValue().getId().equals(id))
                 .toList()
                 .isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean ifKeyIsTaken(){
+        Integer key = Integer.parseInt(args.get(1));
+        if(CollectionManager.getCollection().get(key)!=null){
             return true;
         }
         return false;
